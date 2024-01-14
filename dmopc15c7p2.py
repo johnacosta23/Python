6ceedf68d7cb3d2b183bdefa62bc67cd338a78af
John Acosta <146490109+johnacosta23@users.noreply.github.com>
2024-01-14 22:04:54 +0100

    Create dmopc15c7p2.py

---
+++ dmopc15c7p2.py
@@ -0,0 +1,11 @@
+# This is DMOJ problem dmopc15c7p2
+# https://dmoj.ca/problems/?search=dmopc15c7p2
+
+#The Challenge
+#Count the number of words provided. For this problem, a word is any se-
+#quence of lowercase letters. For example, hello is a word, but so are non-
+#English “words” like bbaabbb
+
+text = input()
+numberofwords = text.count( " ") + 1
+print(numberofwords)
